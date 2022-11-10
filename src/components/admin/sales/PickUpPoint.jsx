8dac8b812d98046a3125467f28cef67ff2214471
Muritala Sodeeq <isholasideeq@gmail.com>
2022-11-10 12:21:16 +0100

    Sideeq commit 1

---
+++ src/components/admin/sales/PickUpPoint.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './PickUpPoint.css'
+export const PickUpPoint = () => {
+  return (
+    <div>PickUpPoint</div>
+  )
+}
+
---
+++ src/components/admin/sales/SellerOrders.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './SellerOrders.css'
+export const SellerOrders = () => {
+  return (
+    <div>SellerOrders</div>
+  )
+}
+
