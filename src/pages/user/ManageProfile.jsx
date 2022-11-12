875de7c82692a86de10b59dba56a01e52edec4b5
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-11-12 22:48:49 +0100

    added route

---
+++ src/pages/user/ManageProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ManageProfile() {
+  return (
+    <div>ManageProfile</div>
+  )
+}
+
+export default ManageProfile
---
+++ src/pages/user/WishList.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const WishList = () => {
+  return <div>WishList</div>;
+};
