543fc62f5e0e84b447aa83c910d9a5a9f8481ae3
Kelvin Moses <moseskelvin4u@gmail.com>
2022-11-19 14:26:53 +0100

    wholesale feature added

--- src/components/wholesale/pages/Allwholesaleproducts.jsx
+++ src/components/wholesale/pages/Allwholesaleproducts.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import ProductTable from '../ProductTable';
-import classes from './InHouseProduct.module.css';
-export const InHouseProduct = () => {
+import AllWholeSaleProductsTable from '../AllWholeSaleProductsTable';
+
+import classes from './Allwholesaleproducts.module.css';
+
+export const AllWholeSaleProducts = () => {
   const [product, setProduct] = useState([]);
   useEffect(() => {
     fetch('https://fakestoreapi.com/products?limit=5')
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         return setProduct(data);
       });
   }, [setProduct]);
   return (
     <div className={classes.container}>
       <div className={classes.product_header}>
-        <span className={classes.title}>All InHouse Product</span>
-        <span>
+        <span className={classes.title}>All wholesale products</span>
+        {/* <span>
           {' '}
           <Link to="">
-            <button className={classes.button}>Add New Product</button>
+            <button className={classes.button}>
+              Add New wholesale Product
+            </button>
           </Link>
-        </span>
+        </span> */}
       </div>
       <div>
-        <ProductTable data={product} />
+        <AllWholeSaleProductsTable data={product} />
       </div>
     </div>
   );
