07b24ca20548d5ad21225eb0f5d23a0d59571f0b
Kelvin Moses <moseskelvin4u@gmail.com>
2022-11-19 14:35:41 +0100

    wholesale feature added

--- src/components/wholesale/pages/Allwholesaleproducts.jsx
+++ src/components/wholesale/pages/Allwholesaleproducts.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import AllWholeSaleProductsTable from '../AllWholeSaleProductsTable';
 
 import classes from './Allwholesaleproducts.module.css';
