42f69ba30111952b49dd7ff6d7221bf9ce6404d4
alialaba <alialaba079@gmail.com>
2022-11-26 00:01:50 -0500

    style:styled  ui component card

--- src/components/sellerDashboard/SellersPackages.jsx
+++ src/components/sellerDashboard/SellersPackages.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import {Card, CardContent , CardActions, Button, Grid, Box, Container, Typography} from '@mui/material'
+import {Card, CardContent , CardActions, Grid, Box, Container, Typography} from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
+import styled from "styled-components";
 
-function createData(img, pack, limit, price) {
-    return { img, pack, limit, price};
+
+const Button = styled.button`
+background-color:#2E294E;
+padding:10px 20px;
+border:1px solid #000;
+font-size: 1rem;
+font-weight:600;
+border-radius:5px;
+color:#fff;
+cursor:pointer;
+`
+
+function createData(img, pack, limit, price,duration) {
+    return { img, pack, limit, price,duration};
   }
 
   const rows=[
-    createData("i.ibb.co/hmnnYz7/platnium.png","Silver", "100 Product Upload Limit", "$10,000"),
-    createData("i.ibb.co/hmnnYz7/platnium.png","Gold", "200 Product Upload Limit", "$20,000"),
-    createData("i.ibb.co/hmnnYz7/platnium.png","Platinum", "500 Product Upload Limit", "$50,000")
+    createData("i.ibb.co/vcRVPFK/silver.png","Silver", "100 Product Upload Limit", "$10,000", "90 Days"),
+    createData("i.ibb.co/LNwJqWK/gold.png","Gold", "200 Product Upload Limit", "$20,000", "240 Days"),
+    createData("i.ibb.co/hmnnYz7/platnium.png","Platinum", "500 Product Upload Limit", "$50,000","365 Days")
   ]
 export const SellersPackages =()=>{
     return(
@@ -28,20 +42,26 @@ export const SellersPackages =()=>{
                     {/* <CardMedia component="img" width="20px" image={`https://${row.img}`} alt={row.pack} /> */}
                     <CardContent>
                         <img style={{width:"100px"}} src={`https://${row.img}`} alt={row.pack}/>
-        <Typography sx={{fontWeight:"bold"}} variant="h5" component="div">
+        <Typography sx={{fontWeight:"bold", my: 3 }} variant="h5" component="div">
           {row.pack}
         </Typography>
-        <Typography  sx={{ mb: 1.5 }} color="text.secondary">
-          {row.limit}
+        <Typography  sx={{ my: 3 , fontWeight:"bold"}} >
+         <CheckIcon sx={{fontSize:"small"}} />  {row.limit}
         </Typography>
-        <Typography sx={{fontWeight:"bold"}}  variant="body2">
+        <Box sx={{display:"flex", alignItems:"center"}}>
+        <Typography sx={{fontWeight:"bold",  marginRight:"5px"}} variant="h4">
           {row.price}
-          <br />
-          {'"a benevolent smile"'}
         </Typography>
+
+        <Typography sx={{fontSize:".9rem"}}>
+          {row.duration}
+        </Typography>
+        </Box>
       </CardContent>
-      <CardActions>
-        <Button align="center" size="small" variant="contained">Purchase Package </Button>
+      <CardActions sx={{ justifyContent: "center" }}>
+       
+        <Button >Purchase Package </Button>
+       
       </CardActions>
       </Card>
       </Grid>
