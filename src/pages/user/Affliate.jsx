0284e43355d0ea844f73b7ae1aa87ea61a806296
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2022-12-01 12:00:00 +0100

    Added user route pages

---
+++ src/pages/user/Affliate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Affliate() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/pages/user/userDashboard.jsx
+++ src/pages/user/userDashboard.jsx
@@ -1,1 +1,10 @@
 // All components for this page are to be created in components/user folder
+import React from 'react'
+
+export default function userDashboard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
