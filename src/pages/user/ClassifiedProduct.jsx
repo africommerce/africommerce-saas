fef96f96c6f7d2e77a4d6f02a45e9c82bfbc5c2d
HedrisTemmyTop <hedristemitope2001@gmail.com>
2022-12-07 14:18:09 +0100

    wallet page half done

--- src/pages/user/ClassifiedProduct.jsx
+++ src/pages/user/ClassifiedProduct.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function ClassifiedProduct() {
+  const [status, setStatus] = useState(true);
+  const toggleButton = () => {
+    setStatus(!status);
+  };
   return (
-    <div>ClassifiedProduct</div>
-  )
+    <div>
+      <div
+        style={{
+          backgroundColor: 'green',
+          width: '100px',
+          height: '50px',
+          borderRadius: '25px',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+        onClick={toggleButton}
+      >
+        <div
+          style={
+            status
+              ? {
+                  backgroundColor: 'black',
+                  width: '45px',
+                  height: '45px',
+                  borderRadius: '50%',
+                  transition: 'all .3s',
+                  transform: 'translateX(3rem)',
+                }
+              : {
+                  backgroundColor: 'black',
+                  width: '45px',
+                  height: '45px',
+                  borderRadius: '50%',
+                  transition: 'all .3s',
+                  transform: 'translateX(0)',
+                }
+          }
+        ></div>
+      </div>
+    </div>
+  );
 }
 
-export default ClassifiedProduct
+export default ClassifiedProduct;
--- src/pages/user/Wallet.jsx
+++ src/pages/user/Wallet.jsx
@@ -1,7 +1,143 @@
 import React from 'react';
-
+import DataBox from './components/dataBox';
+import PurpleBox from './components/purpleBox';
+import WhiteBox from './components/whiteBox';
+import classes from '../../styles/affiliate.module.css';
+import Box from './components/Box';
 function Wallet() {
-  return <div style={{ flex: '1' }}>Wallet</div>;
+  const headers = ['#', 'Date', 'Amount', 'Payment Method', 'Approval'];
+  const state = [
+    {
+      date: '01-06-2022',
+      amount: '$5.000',
+      method: 'refund',
+      approval: 'N/A',
+    },
+    {
+      date: '20-04-2022',
+      amount: '$5.000',
+      method: 'refund',
+      approval: 'N/A',
+    },
+    {
+      date: '09-03-2020',
+      amount: '$20.000',
+      method: 'refund',
+      approval: 'N/A',
+    },
+    {
+      date: '09-03-2020',
+      amount: '$20.000',
+      method: 'refund',
+      approval: 'N/A',
+    },
+    {
+      date: '09-03-2020',
+      amount: '$20.000',
+      method: 'refund',
+      approval: 'pending',
+    },
+  ];
+  return (
+    <div style={{ flex: '1' }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <PurpleBox symbol="$" value="$1,703.300" caption="walllet Balance" />
+        <WhiteBox icon="plus" caption="Recharge Walley" />
+        <WhiteBox icon="plus" caption="Recharge Walley" />
+      </div>
+      <div>
+        <div
+          className={classes.PaymentContent}
+          style={{ borderRadius: '10px' }}
+        >
+          <div className={classes.PaymentHistoryBox}>
+            <div
+              className={[
+                classes.PaymentHistoryBoxHead,
+                classes.StatsHead,
+              ].join(' ')}
+            >
+              Wallet Recharge History
+            </div>
+          </div>
+          <div style={{ padding: '0px 20px' }}>
+            <div className={classes.PaymentMain}>
+              {headers.map((head, i) => {
+                return (
+                  <div
+                    key={i}
+                    style={i === headers.length - 1 ? { flex: '1' } : null}
+                    className={
+                      i === 0 ? classes.PaymentSN : classes.PaymentDate
+                    }
+                  >
+                    {head}
+                  </div>
+                );
+              })}
+            </div>
+            {state.map((data, i) => {
+              return (
+                <div
+                  className={classes.PaymentMain}
+                  key={i}
+                  style={
+                    state.length - 1 === i
+                      ? { paddingBottom: '50px', borderBottom: '0px' }
+                      : null
+                  }
+                >
+                  <div
+                    style={{ fontSize: '12px' }}
+                    className={classes.PaymentSN}
+                  >
+                    {i + 1}
+                  </div>
+                  <div
+                    style={{ fontSize: '12px' }}
+                    className={classes.PaymentDate}
+                  >
+                    {data.date}
+                  </div>
+                  <div
+                    style={{ fontSize: '12px' }}
+                    className={classes.PaymentAmount}
+                  >
+                    {data.amount}
+                  </div>
+                  {data.method ? (
+                    <div
+                      style={{ fontSize: '12px', textTransform: 'capitalize' }}
+                      className={classes.PaymentMethod}
+                    >
+                      {data.method}
+                    </div>
+                  ) : null}
+                  {data.status ? (
+                    <div className={classes.PaymentMethod}>
+                      <div className={[classes[data.status]].join(' ')}>
+                        <span
+                          style={{ display: 'flex', justifyContent: 'center' }}
+                        >
+                          {' '}
+                          {data.status}
+                        </span>
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Wallet;
