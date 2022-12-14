6ff782fa5d23e63b53077cef01a787840a85e3d8
Kelvin Moses <moseskelvin4u@gmail.com>
2022-12-14 08:58:43 +0100

    http hook added

---
+++ src/adapter/hooks/useHttp.jsx
@@ -0,0 +1,37 @@
+import { useState, useCallback } from 'react';
+export const baseUrl = () => 'https://africom-test-api.cyclic.app';
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
